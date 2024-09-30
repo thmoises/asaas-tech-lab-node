c742c2654141d5a97ee21ab9604e26026d1d7616
Vinícius Wille <viniciuswillealves@gmail.com>
2024-09-30 16:52:35 -0300

    feat: Adicionando rota e documentação

--- src/routes/payment-route.ts
+++ src/routes/payment-route.ts
@@ -14,6 +14,65 @@ const router = Router();
 
 /**
  * @swagger
+ * /payments:
+ *   get:
+ *     summary: Lista todos os registros de pagamento
+ *     tags: [Payment]
+ *     responses:
+ *       200:
+ *         description: Lista de pagamentos retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 totalCount:
+ *                   type: number
+ *                   description: Número total de registros.
+ *                   example: 100
+ *                 limit:
+ *                   type: number
+ *                   description: Limite de registros por página.
+ *                   example: 10
+ *                 offset:
+ *                   type: number
+ *                   description: Deslocamento para paginação.
+ *                   example: 0
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       value:
+ *                         type: number
+ *                         description: Valor.
+ *                         example: 100.50
+ *                       dueDate:
+ *                         type: string
+ *                         format: date-time
+ *                         description: Data de vencimento.
+ *                         example: 2024-08-15
+ *                       description:
+ *                         type: string
+ *                         description: Descrição do pagamento.
+ *                         example: Pagamento referente ao serviço X.
+ *                       billingType:
+ *                         type: string
+ *                         description: Tipo de cobrança.
+ *                         example: BOLETO
+ *                       status:
+ *                         type: string
+ *                         description: Status do pagamento.
+ *                         example: PENDING
+ *                       customer:
+ *                         type: string
+ *                         description: ID do cliente.
+ *                         example: cus_000006227230
+ */
+router.get('/payments', (req: Request, res: Response) => paymentController.findAll(req, res));
+
+/**
+ * @swagger
  * /payment:
  *   post:
  *     summary: Cria um registro de pagamento
