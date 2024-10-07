3bb5687143e9b2de85476d3adee0ec6441fe4062
Glauber Funez <glauber.funez@asaas.com.br>
2024-10-07 17:34:59 -0300

    Cria middleware

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -1,23 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
-import {getIssuer} from "../config/auth";
-import dataSource from "../models";
-import {JwtPayload} from "jsonwebtoken";
-import UserServices from "../services/user-services";
-import {object} from "yup";
+import { getIssuer } from '../config/auth';
+import { JwtPayload } from 'jsonwebtoken';
+import UserServices from '../services/user-services';
 
-export async function middlewareValidarJWT(req: Request, res: Response, next: NextFunction) {
-    const jwt = req.headers.authorization;
-    if (!jwt) return res.status(403).end();
+export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const jwt = req.headers.authorization;
+  if (!jwt) return res.status(403).end();
 
-    try {
-        const decode = getIssuer(jwt) as JwtPayload;
-        const user = await new UserServices().findByEmail(decode.iss as string);
+  try {
+    const decode = getIssuer(jwt) as JwtPayload;
+    const user = await new UserServices().findByEmail(decode.iss as string);
 
-        if (!user.dataValues || !Object.keys(user.dataValues).length) return res.status(403).end();
+    if (!user.dataValues || !Object.keys(user.dataValues).length) return res.status(403).end();
 
-        (req as any).user = user.dataValues;
-        next();
-    } catch (err) {
-        res.status(403).end();
-    }
+    (req as unknown).user = user.dataValues as Record<string, unknown>;
+    next();
+  } catch {
+    res.status(403).end();
+  }
 }
